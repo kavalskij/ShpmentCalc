3560bca80e00491d8b94abf9d2bfa85753bb8ef5
Aleksandrs Muravjovs <kavalskij@gmail.com>
2019-02-05 11:23:00 +0200

    Initial Commit

---
+++ Shipment Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shipment Calc
+//
+//  Created by Aleksandrs Muravjovs on 05/02/2019.
+//  Copyright © 2019 Aleksandrs Muravjovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
